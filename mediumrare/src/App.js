ac9a6e41b5ebc384a8222781d31ba60aef29fdaf
Alex Delgado <delgadoalex@icloud.com>
2019-04-30 12:16:05 -0500

    Building the layout <html> -w

--- mediumrare/src/App.js
+++ mediumrare/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Medium Rare Records</h1>
+    <div className="tc">
+      
     </div>
   );
 }
