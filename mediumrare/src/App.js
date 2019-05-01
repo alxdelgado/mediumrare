6ff6e40804ad8ba9a978bb68c2edac7f3cbbcc57
Alex Delgado <delgadoalex@icloud.com>
2019-05-01 15:41:48 -0500

    Three components built, and rendered. Next steps are to build out show page.  -w

--- mediumrare/src/App.js
+++ mediumrare/src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+import './Home.js';
+import './Records.js';
+import './News.js';
+import './Artists.js';
 
-function App() {
-  return (
-    <div className="tc">
-      
-    </div>
-  );
-}
+
+
+class App extends Component {
+   render() {
+      return (
+         <Router>
+            <div>
+               <h2>Medium Rare Records</h2>
+               <nav className="main-nav navbar navbar-expand-lg navbar-light bg-light">
+                  <ul>
+                     <li><Link to={'/Records'} className="nav-link">Records</Link></li>
+                     <li><Link to={'/Artists'} className="nav-link">Artists</Link></li>
+                     <li><Link to={'/News'} className="nav-link">News</Link></li>
+                  </ul>
+               </nav>
+            </div>
+         </Router>
+      )
+   }
+};
 
 export default App;
