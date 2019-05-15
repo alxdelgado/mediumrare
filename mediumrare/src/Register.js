86ec0772d43edf8e86b4aac7ed86723b9e2bf3b2
Alex Delgado <delgadoalex@icloud.com>
2019-05-15 13:18:07 -0500

    Login and register routes created.

---
+++ mediumrare/src/Register.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+   render() {
+      return (
+         <h4>Register</h4>
+      )
+   }
+};
+
+export default Register;
